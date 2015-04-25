2b27ba6d48204996d091f1e0112548e35c9ec758
theServerGod <the.server.god@gmail.com>
2015-04-25 10:17:00 +1000

    Updated timedifference layout and UI, minor misc UI tweaks

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -11,7 +11,8 @@ app.controller('AppController', [
 function($scope, $mdSidenav, $timeout, $mdDialog, menu, $location, $route, $rootScope, $log) {
 	var self = this;
 
-	$scope.SITE_ROOT = "http://theservergod.github.io/Utilitron/"; // FIXME - Add this as a proper constant
+	$scope.SITE_ROOT = 'http://theservergod.github.io/Utilitron/'; // FIXME - Add this as a proper constant
+	//$scope.SITE_ROOT = '/'; // Uncomment to use development base URI
 
 	// Expose $route var to app and child controllers - used mainly for $route.current
 	$scope.$route = $route;
