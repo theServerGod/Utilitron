a99e17b2978bcb866d075ad5614503a502d16a7f
theServerGod <the.server.god@gmail.com>
2015-04-26 10:12:40 +1000

    Added date difference with minor updates to time diff functionality

--- app/controllers/time-date.js
+++ app/controllers/time-date.js
@@ -19,10 +19,19 @@ function($scope) {
       diff.sec = end.diff(start, 'seconds');
       diff.min = end.diff(start, 'minutes');
       diff.hrs = end.diff(start, 'hours', true);
-      diff.text = Math.floor(diff.hrs) + ' hours, ' + (diff.min - (Math.floor(diff.hrs)*60)) + ' minutes, ' + (diff.sec - (diff.min*60)) + ' seconds';
+      diff.text = humanizeTimeDiff(diff.hrs, diff.min, diff.sec);
       return diff;
     } else
       return null;
+  }
+
+  /* Given total time difference as hours, minutes, and seconds output human-readable string with correct plurality */
+  function humanizeTimeDiff(hrs, min, sec) {
+    // Calculate the time as a combined format
+    var h = Math.floor(hrs);
+    var m = (min - (hrs*60));
+    var s = (sec - (min*60));
+    return h + (h === 1 ? ' hour, ' : ' hours, ') + m + (m === 1 ? ' minute, ' : ' minutes, ') + s + (s === 1 ? ' second' : ' seconds');
   }
 
   $scope.$watch('time', function(newValue, oldValue) {
@@ -40,7 +49,33 @@ function($scope) {
 app.controller('DateDifferenceController', [
   '$scope',
 function($scope) {
+  var self = this;
+  $scope.date = {};
 
+  function initDates() {
+    $scope.date.start = new Date(moment().set('hours', 0).set('minutes', 0).set('seconds', 0).set('milliseconds', 0));
+    $scope.date.end = $scope.date.start;
+  }
+
+  function calcDateDiff(start, end) {
+    if (start && end) {
+      var diff = {};
+      start = moment(start);
+      end = moment(end);
+      diff.years = end.diff(start, 'years', true);
+      diff.months = end.diff(start, 'months', true);
+      diff.days = end.diff(start, 'days');
+      diff.text = moment(end).from(moment(start), true) === 'a few seconds' ? null : moment(end).from(moment(start), true);
+      return diff;
+    } else
+      return null;
+  }
+
+  $scope.$watch('date', function(newValue, oldValue) {
+    $scope.date.diff = calcDateDiff(newValue.start, newValue.end);
+  }, true);
+
+  initDates();
 }]);
 // }}}
 // Timezone {{{
