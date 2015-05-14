244ece7a9eb3234700012a9522dc6c1bfefa1c75
theServerGod <the.server.god@gmail.com>
2015-05-15 01:07:47 +1000

    REFACTOR: Added routes.js file, moved script loads to bottom of <body>

---
+++ app/routes.js
@@ -0,0 +1,62 @@
+// Routes {{{
+app.config([
+	'$routeProvider',
+	'$mdThemingProvider',
+function($routeProvider, $mdThemingProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'app/views/home.html'
+		})
+		.when('/getting-started', {
+			templateUrl: 'app/views/getting-started.html',
+			title: 'Getting Started'
+		})
+		.when('/timedate/timedifference', {
+			templateUrl: 'app/views/timedate/timedifference.html',
+			controller: 'TimeDifferenceController',
+			title: 'Time Difference'
+		})
+		.when('/timedate/datedifference', {
+			templateUrl: 'app/views/timedate/datedifference.html',
+			controller: 'DateDifferenceController',
+			title: 'Date Difference'
+		})
+		.when('/timedate/timezone', {
+			templateUrl: 'app/views/timedate/timezone.html',
+			controller: 'TimezoneController',
+			title: 'Timezone Finder'
+		})
+		.when('/unitconversion', {
+			templateUrl: 'app/views/unit-conversion.html',
+			controller: 'UnitConversionController',
+			title: 'Unit Conversion'
+		})
+		.when('/calculator', {
+			templateUrl: 'app/views/calculator.html',
+			title: 'Calculator'
+		})
+		.when('/language/dictionary', {
+			templateUrl: 'app/views/language/dictionary.html',
+			title: 'Dictionary'
+		})
+		.when('/language/grammar', {
+			templateUrl: 'app/views/language/grammar.html',
+			title: 'Grammar Reference'
+		});
+
+	// Error pages {{{
+	$routeProvider
+		.when('/404', {
+			templateUrl: 'app/views/error/404.html',
+			//title: 'Page Not Found'
+		});
+	// }}}
+
+	$routeProvider.otherwise('/404');
+
+	$mdThemingProvider.theme('dark', 'default')
+		.primaryPalette('yellow')
+		.dark();
+}]);
+
+// }}}
